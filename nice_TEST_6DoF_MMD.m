3104d45d67ec6b6eef5d8851954bb0252bf41ec7
StravopodisNikos <stravnik.rob@gmail.com>
2019-05-04 21:29:14 +0300

    well structured TEST file for 6 DOF MMD only with P2P motion test

--- nice_TEST_6DoF_MMD.m
+++ nice_TEST_6DoF_MMD.m
@@ -18,14 +18,14 @@ RefRobotName = '/home/nikos/matlab_ws/modular_dynamixel/reference_MMD_inverse.ur
 TestRobotName = '/home/nikos/matlab_ws/modular_dynamixel/x_test_inverse_MMD1.urdf';
 [TestRobot,TestFig,TestConfig,NumDoF] = ImportRobotTestAnatomyModel(TestRobotName);
 
-%% Breaks inital robot models to links
+%% Breaks initial robot models to links
 [RefRobot_links,CoM_RefRobot_links,gsli0,gsbj0,M0b_CoM,M0s_CoM] = robot_links_subtree_new(RefRobot,RefConfig,NumDoF);
 [TestRobot_links,CoM_TestRobot_links,gsli1,gsbj1,M1b_CoM,M1s_CoM] = robot_links_subtree_new(TestRobot,TestConfig,NumDoF);
 
 %%  Extract Geometry points for each body from FKP 
 %   in reference anatomy and zero configuration!!!
-[pi_ref,gst0] = ExtractGeometryPoints(RefRobot,RefConfig);
-[pi_test,gst1] = ExtractGeometryPoints(TestRobot,TestConfig);
+[pi_ref,gst0] = ExtractGeometryPoints_6DoF(RefRobot,RefConfig);
+[pi_test,gst1] = ExtractGeometryPoints_6DoF(TestRobot,TestConfig);
 
 %% Specify reference anatomy-With regard to URDF Pseudo Configuration!!!
 tpi_ref = [0 0 0 0 0 0]';% Reference Anatomy values are theta_p_i's specified in MMDinertia_scale_xacro_FixedAnatomy.xacro and in the urdf built from it and given in l.10
@@ -49,12 +49,13 @@ gd_ref = getTransform(RefRobot,RefConfig,TOOL_ref);
 gd_ref_POE = MMD_POE_FKP(RefConfig,xi_ai,tpi_ref,xi_pi,gst0);
 gd_test = getTransform(TestRobot,TestConfig,TOOL_test); 
 gd_test_POE = MMD_POE_FKP(TestConfig,xi_ai,tpi1,xi_pi,gst0);
+
 gd = gd_test_POE; % User defined desired cartesian position and orientation
 % 2. MATLAB IKP solver
 RefIKS = robotics.InverseKinematics('RigidBodyTree',RefRobot); % create matlab solver
-[RefConfigSol,RefSolInfo] = RefIKS('TOOL',gd,[0.01 0.01 0.01 0.01 0.01 0.01]',[0 0 0 0 0 0]');
+[RefConfigSol,RefSolInfo1] = RefIKS('TOOL',gd,[0.01 0.01 0.01 0.01 0.01 0.01]',[0 0 0 0 0 0]');
 TestIKS = robotics.InverseKinematics('RigidBodyTree',TestRobot); % create matlab solver
-[TestConfigSol,TestSolInfo] = TestIKS('TOOL',gd,[0.01 0.01 0.01 0.01 0.01 0.01]',[0 0 0 0 0 0]');
+[TestConfigSol,TestSolInfo1] = TestIKS('TOOL',gd,[0.01 0.01 0.01 0.01 0.01 0.01]',[0 0 0 0 0 0]');
 % 3. POE solvers using screw Theory only for 3DoF - to be developed next
 % row spherical wrist last 3 axes
 
@@ -67,3 +68,37 @@ theta_dotdot = [10 0 0 0 0 0]';
 
 [M0_POE,V0_POE,N0_POE,M0m,V0m,N0m,JointTorque0_POE,JointTorq0M] = InverseDynamicProblemPOE_6DoF_MMD(RefRobot,RefConfigSol,theta_dot,theta_dotdot,M0b_CoM,M0s_CoM,Jssli_ref,Jbsli_ref,xi_ai, Pi_ref, gsli0);
 [M1_POE,V1_POE,N1_POE,M1m,V1m,N1m,JointTorque1_POE,JointTorq1M] = InverseDynamicProblemPOE_6DoF_MMD(TestRobot,TestConfigSol,theta_dot,theta_dotdot,M0b_CoM,M0s_CoM,Jssli_test,Jbsli_test,xi_ai, Pi_test, gsli0);
+
+%% P2P motion & PD control task in ConfigSpace with Dynamic Performance Evaluation
+% Cartesian Space
+gdi = gd;
+gdf = gd; gdf(1,4) = gdf(1,4)+0.1; gdf(2,4) = gdf(2,4)+0.1; % 45deg angle in xy plane
+
+% Configuration Space
+RefIKS = robotics.InverseKinematics('RigidBodyTree',RefRobot); % create matlab solver
+[RefConfigSol2,RefSolInfo2] = RefIKS('TOOL',gdf,[0.01 0.01 0.01 0.01 0.01 0.01]',[0 0 0 0 0 0]');
+TestIKS = robotics.InverseKinematics('RigidBodyTree',TestRobot); % create matlab solver
+[TestConfigSol2,TestSolInfo2] = TestIKS('TOOL',gdf,[0.01 0.01 0.01 0.01 0.01 0.01]',[0 0 0 0 0 0]');
+
+% Task is: (x,y)=(0.1966,-0.0972)->(0.2966,0.0028) with ct z
+% Reference anatomy: RefConfigSol1 -> RefConfigSol2
+% Test anatomy: TestConfigSol1 -> TestConfigSol2
+% PD computed torque control
+%% Execute task for reference anatomy
+qi = RefConfigSol; % Initial condition for joint positions
+qd = RefConfigSol2;
+dqd = [0 0 0 0 0 0]'; % define the joint velocity when achieving gd. Always zero for P2P motion!
+ddqd = [0 0 0 0 0 0]'; % define the joint acceleration when achieving gd. Arbitrary for now!
+anatomy_cnt = 1;
+[tvec1,dy1] = control_6DoF_Metamorphic_Dynamixel(RefRobot,qi,qd,dqd,ddqd,anatomy_cnt);
+DCI1 = DCI;
+Torques1 = torque;
+%% Execute task for test anatomy
+qi = TestConfigSol; % Initial condition for joint positions
+qd = TestConfigSol2;
+dqd = [0 0 0 0 0 0]'; % define the joint velocity when achieving gd. Always zero for P2P motion!
+ddqd = [0 0 0 0 0 0]'; % define the joint acceleration when achieving gd. Arbitrary for now!
+anatomy_cnt = 2;
+[tvec2,dy2] = control_6DoF_Metamorphic_Dynamixel(TestRobot,qi,qd,dqd,ddqd,anatomy_cnt);
+DCI2 = DCI;
+Torques2 = torque;

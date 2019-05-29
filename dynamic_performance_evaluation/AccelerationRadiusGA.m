008575346e590c685b93e7d73d1e70c818b7dd7c
StravopodisNikos <stravnik.rob@gmail.com>
2019-05-29 19:42:58 +0300

    Calls optimization solvers for DPM optimization in a 6DoF metamorphic manipulator

--- dynamic_performance_evaluation/AccelerationRadiusGA.m
+++ dynamic_performance_evaluation/AccelerationRadiusGA.m
@@ -1,8 +1,5 @@
 function [x,fval,exitflag,output] = AccelerationRadiusGA(tp1, xi_ai, xi_pi, Pi_test, gst0, gsli0, M0b_CoM)
-% Workspace Specification for single anatomy
-% For given tpi, we find the area inside configuration space that, a (high)
-% minimum number of TCP acceleration is achieved
-% tpi is global in order to be passed inside Objective, Constraint functions
+
 
 %% Uncomment desired call for solver
 
@@ -26,10 +23,32 @@ function [x,fval,exitflag,output] = AccelerationRadiusGA(tp1, xi_ai, xi_pi, Pi_t
 % LB = [-1.57 -1.57 1 1 1 1 1 1];
 % UB = [1.57 1.57 5 5 5 5 5 5];
 %% 3 for Wd
-FitnessFunction = @(x)MaxWd(x, xi_ai, xi_pi, gst0, gsli0, M0b_CoM);
-nvars = 8; % q2,3 tpi
-LB = [-1.57 -1.57 1 1 1 1 1 1];
-UB = [1.57 1.57 5 5 5 5 5 5];
+% FitnessFunction = @(x)MaxWd(x, xi_ai, xi_pi, gst0, gsli0, M0b_CoM);
+% nvars = 8; % q2,3 tpi
+% LB = [-1.57 -1.57 1 1 1 1 1 1];
+% UB = [1.57 1.57 5 5 5 5 5 5];
+%% 4 for GDII
+% FitnessFunction = @(x)GlobalDynamicIsotropyIndex(x, xi_ai, xi_pi, gst0, gsli0, M0b_CoM);
+% ConstraintFunction = @(x)ConGlobalDynamicIsotropyIndex(x, xi_ai, xi_pi, gst0, gsli0, M0b_CoM);
+% 
+% nvars = 8; % q2,3 tpi
+% LB = [-1.57 -1.57 1 1 1 1 1 1];
+% UB = [1.57 1.57 5 5 5 5 5 5];
+%% 5 for GlobalMaxWd
+% FitnessFunction = @(x)GlobalMaxWd(x, xi_ai, xi_pi, gst0, gsli0, M0b_CoM);
+% ConstraintFunction = @(x)ConGlobalMaxWd(x, xi_ai, xi_pi, gst0, gsli0, M0b_CoM);
+% 
+% nvars = 8; % step_a2,3 - tpi
+% LB = [0.2 0.2 1 1 1 1 1 1];
+% UB = [0.4 0.4 5 5 5 5 5 5];
+%% 6 for GlobalMinDCI - gamultiobj doesn't accept integer constraints!!!!
+FitnessFunction = @(x)[GlobalMinDCI1(x, xi_ai, xi_pi, gst0, gsli0, M0b_CoM),GlobalMinDCI2(x, xi_ai, xi_pi, gst0, gsli0, M0b_CoM)];
+% FitnessFunction2 = @(x)GlobalMinDCI2(x, xi_ai, xi_pi, gst0, gsli0, M0b_CoM);
+ConstraintFunction = @(x)ConGlobalMinDCI(x, xi_ai, xi_pi, gst0,gsli0,M0b_CoM);  
+
+nvars = 8; % step_a2,3 - tpi
+LB = [0.2 0.2 1 1 1 1 1 1];
+UB = [0.4 0.4 5.999 5.999 5.999 5.999 5.999 5.999];
 %% GA calls for single anatomy
 % tic
 % options = optimoptions('ga','Generations',200,'PopulationSize',100,'Display','iter','MutationFcn',{'mutationadaptfeasible'},'StallGenLimit',20);
@@ -39,17 +58,35 @@ UB = [1.57 1.57 5 5 5 5 5 5];
 % [x,fval,exitflag,output] = ga(FitnessFunction,nvars,[],[],[],[],LB,UB,[],[],options);
 % toc
 %% GA calls for anatomy determination
-%% 1
+%% 1 is global measure
 % IntCon = [5,6,7,8,9,10];
 % options = optimoptions('ga','Generations',400,'PopulationSize',100,'Display','iter','StallGenLimit',50,'UseParallel', true, 'UseVectorized', false);
 % tic
 % [x,fval,exitflag,output] = ga(FitnessFunction,nvars,[],[],[],[],LB,UB,ConstraintFunction,IntCon,options);
 % toc
-%% 2+3
+%% 2+3 are local measures
+% IntCon = [3,4,5,6,7,8];
+% options = optimoptions('ga','Generations',400,'PopulationSize',400,'Display','iter','StallGenLimit',50,'UseParallel', true, 'UseVectorized', false);
+% tic
+% [x,fval,exitflag,output] = ga(FitnessFunction,nvars,[],[],[],[],LB,UB,[],IntCon,options);
+% toc
+%% 4 is global measures
+% IntCon = [3,4,5,6,7,8];
+% options = optimoptions('ga','Generations',500,'PopulationSize',1000,'Display','iter','StallGenLimit',100,'UseParallel', true, 'UseVectorized', false);
+% tic
+% [x,fval,exitflag,output] = ga(FitnessFunction,nvars,[],[],[],[],LB,UB,ConstraintFunction,IntCon,options);
+% toc
+%% 5 is global measure
+% IntCon = [3,4,5,6,7,8];
+% options = optimoptions('ga','Generations',400,'PopulationSize',200,'Display','iter','FunctionTolerance',1e-12,'StallGenLimit',50,'UseParallel', true, 'UseVectorized', false);
+% tic
+% [x,fval,exitflag,output] = ga(FitnessFunction,nvars,[],[],[],[],LB,UB,ConstraintFunction,IntCon,options);
+% toc
+%% 6 is global measure
 IntCon = [3,4,5,6,7,8];
-options = optimoptions('ga','Generations',400,'PopulationSize',400,'Display','iter','StallGenLimit',50,'UseParallel', true, 'UseVectorized', false);
+options = optimoptions('gamultiobj','Generations',400,'PopulationSize',200,'Display','iter','FunctionTolerance',1e-12,'StallGenLimit',50,'UseParallel', true, 'UseVectorized', false);
 tic
-[x,fval,exitflag,output] = ga(FitnessFunction,nvars,[],[],[],[],LB,UB,[],IntCon,options);
+[x,fval,exitflag,output] = gamultiobj(FitnessFunction,nvars,[],[],[],[],LB,UB,ConstraintFunction,options);
 toc
 %% fmincon - local minimum danger
 % x0 = [0.5 -0.5 -0.5 -0.5];

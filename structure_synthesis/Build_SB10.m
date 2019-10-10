dfb0cb7dee484724a3106bf3e1e9e30dc9387282
StravopodisNikos <stravnik.rob@gmail.com>
2019-10-10 14:09:40 +0300

    0_10 is working, next step is 0_110

--- structure_synthesis/Build_SB10.m
+++ structure_synthesis/Build_SB10.m
@@ -1,5 +1,6 @@
-function [Si] = Build_SB10(Sim1,Si0,R,P,XIim1,SBn,p2fig)
+function [Si] = Build_SB10(Sim1,Si0,R,P,SBn,p2fig,ti,Xi1)
 % Solves FKP for Structural Block C01, given the previous twists and tfs
+% Works ONLY for 3 DoF with the predefined ruleset
 % INPUT: 1.Sim1 is the struct that contains the previous STRUCTURAL BLOCK INFO
 %        after transformed due to structure changes
 %        2.Si0 is the struct that contains the current STRUCTURAL BLOCK INFO
@@ -8,9 +9,7 @@ function [Si] = Build_SB10(Sim1,Si0,R,P,XIim1,SBn,p2fig)
 %        of the current STRUCTURAL BLOCK
 %        Si.gsli is the new zero tf of active joint
 %        Si.expi(:,:,2) for j,i twists
-%        Si.xi is the new active twist
-%        Si.xj
-%        Si.xk
+%        Si.Xi is the new active, synthetic and pseudo twists
 %        Si.Js
 
 %% Synthetic Joint is parameterized to exponential
@@ -32,29 +31,35 @@ g_slk = exp_lk*Sim1.Cg; % this is the new g-s-lk after synthetic tf changes aka 
 % % the rigid motion of the structure change induced by synthetic_joint:frame_PseudoConnector1a
 
 % The twists of SB10 are re-calculated due to Synthetic Joint
-wjn = exp_wmegaR*Si0.wi(:,2); g_sP1b = g_slk*Si0.g0(:,:,8);
+wjn = exp_wmegaR*Si0.wi(:,1); g_sP1b = g_slk*Si0.g0(:,:,8);
 xjn = createtwist(wjn,g_sP1b(1:3,4)); %ξj'
-wi1n = exp_wmegaR*Si0.wi(:,3); g_sf1 = g_slk*Si0.g0(:,:,8)*Si0.g0(:,:,5);
+wi1n = exp_wmegaR*Si0.wi(:,2); g_sf1 = g_slk*Si0.g0(:,:,8)*Si0.g0(:,:,5);
 xi1n = createtwist(wi1n,g_sf1(1:3,4)); %ξi+1'
+Xi_for_struct(:,1) = xjn;
+Xi_for_struct(:,2) = xi1n;
 
 %% Fwd Kinematic mapping
-ti = [0 0]'; % from here i control the 2 angles: 1:passive1 2:active1
 %% Extract new zero tfs after structure changes
 gn(:,:,1) = g_slk*Si0.g0(:,:,8); % g_s_lj0n = g_s_lk*g_lk_lj0
 gn(:,:,2) = g_slk*Si0.g0(:,:,8)*Si0.g0(:,:,5); % g_s_li10n
-gn(:,:,3) = inv(Sim1.gsli)*gn(:,:,1); % g_li_lj0
+gn(:,:,3) = inv(Sim1.g0)*gn(:,:,1); % g_li_lj0
 gn(:,:,4) = inv(gn(:,:,1))*gn(:,:,2); % g_lj_li10
-gn(:,:,5) = inv(Sim1.gsli)*gn(:,:,2); % g_li_li10
+gn(:,:,5) = inv(Sim1.g0)*gn(:,:,2); % g_li_li10
+
+NEW_S_FRAME = g_slk*Si0.Cg; % new {s} frame for next body is the gst of previous set of SBs'
 
 % Now, extract new relative twists
-xi_j_new = inv(ad(Sim1.gsli))*xjn;
-xi_i1_new = inv(ad(Sim1.gsli))*xi1n;
+xi_j_new = inv(ad(Sim1.g0))*xjn;
+xi_i1_new = inv(ad(Sim1.g0))*xi1n;
 xj_i1_new = inv(ad(gn(:,:,1)))*xi1n;
 
 % After new structure is completed, the passive-active expos are
 % constructed as usual
-expj = twistexp(xjn, ti(1));
-expi = twistexp(xi1n, ti(2));
+exp1 = twistexp(Xi1,ti(1));
+expj = twistexp(xjn, ti(2));
+expi = twistexp(xi1n, ti(3));
+exp_for_struct(:,:,1) = expj;
+exp_for_struct(:,:,2) = expi;
 
 % Here is POE FKM for new structure
 % ONLY for 3DOF
@@ -71,12 +76,13 @@ elseif SBn==3
         gni = exp1*Sim1.expi(:,:,1)*Sim1.expi(:,:,2)*expj*expi*gn(:,:,2);        
     end
 end
-
+gsn(:,:,1) = gnj;
+gsn(:,:,2) = gni;
 %% Relative POE FKM
 g_li_li1 = twistexp(xi_j_new,ti(1))*gn(:,:,3)*twistexp(xj_i1_new,ti(2))*gn(:,:,4); % for synthetic+metamorphic
 %% Relative twists
 [xi_i1_abs th_i_i1_abs] = homtotwist(g_li_li1); % the absolute transformation twist- READS structure change
-g = g_li_li1a*inv(gn(:,:,5));
+g = g_li_li1*inv(gn(:,:,5));
 [xi_i1_rel th_i_i1_rel] = homtotwist(g); % the relative transformation twist - READS pseudojoint change
 
 %% Spatial Jacobian
@@ -102,5 +108,11 @@ xj_graph = drawtwist(Js(:,2)); hold on;
 xi1_graph = drawtwist(Js(:,3)); hold on;
 
 %% Build OUTPUT struct
-
+f1 = 'g0'; v1 = gsn;
+f2 = 'Cg'; v2 = NEW_S_FRAME; %
+f3 = 'expi'; v3 = exp_for_struct;
+f4 = 'xi'; v4 = Xi_for_struct;
+f5 = 'Sframe'; v5 = NEW_S_FRAME;
+f6 = 'Js'; v6 = Js;
+Si = struct(f1,v1,f2,v2,f3,v3,f4,v4,f5,v5,f6,v6);
 end

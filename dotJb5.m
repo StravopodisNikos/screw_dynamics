3d53c043b85150e7d6f07dd5ab184dceb7c67d76
StravopodisNikos <stravnik.rob@gmail.com>
2019-10-29 13:02:22 +0200

    First Time Derivative of S-B Jacobians

--- dotJb5.m
+++ dotJb5.m
@@ -2,12 +2,12 @@ function dotJb5 = dotJb5(qdot,Jb)
 % Calculates the first time derivative of Body Jacobian for 6 DoF serial
 % manipulator
 
-xib1 = Jb(1,:);
-xib2 = Jb(2,:);
-xib3 = Jb(3,:);
-xib4 = Jb(4,:);
-xib5 = Jb(5,:);
-xib6 = Jb(6,:);
+xib1 = Jb(:,1);
+xib2 = Jb(:,2);
+xib3 = Jb(:,3);
+xib4 = Jb(:,4);
+xib5 = Jb(:,5);
+xib6 = Jb(:,6);
 
 dxib1 = dotxib1(qdot,xib1,xib2,xib3,xib4,xib5,xib6);
 dxib2 = dotxib2(qdot,xib2,xib3,xib4,xib5,xib6);
--- dotJs5.m
+++ dotJs5.m
@@ -2,12 +2,12 @@ function dotJs5 = dotJs5(qdot,Js)
 % Calculates the first time derivative of Spatial Jacobian for 6 DoF serial
 % manipulator
 
-xi1 = Js(1,:);
-xi2 = Js(2,:);
-xi3 = Js(3,:);
-xi4 = Js(4,:);
-xi5 = Js(5,:);
-xi6 = Js(6,:);
+xi1 = Js(:,1);
+xi2 = Js(:,2);
+xi3 = Js(:,3);
+xi4 = Js(:,4);
+xi5 = Js(:,5);
+xi6 = Js(:,6);
 
 dxi1 = [0 0 0 0 0 0]';
 dxi2 = dotxi2(qdot,xi1,xi2);

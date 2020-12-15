6b81faef1fa8e2b4267f4e808430c61e8c6ed613
StravopodisNikos <stravnik.rob@gmail.com>
2020-12-15 17:03:27 +0200

    working on numeric gravity matrix

--- calculate_gravity_matrix_numer.m
+++ calculate_gravity_matrix_numer.m
@@ -1,12 +1,15 @@
-function [N] = calculate_gravity_matrix_numer(dof_string, gsli, mi, Vold)
-%
+function [N] = calculate_gravity_matrix_numer(dof_string, gsli, mi, Vold, dq)
+% Numerically calculates the gradient of Gravity matrix @ each C-space
+% point. gsli is comuted at each C-space point and passed with dq vector to
+% this function
+
 switch dof_string
     case '3dof'
         n_Dof = 3;
     case '6dof'
         n_Dof = 6;
 end
-
+ 
 g = -9.8067; % [m/s^2]
 
 V = zeros(n_Dof,1);
